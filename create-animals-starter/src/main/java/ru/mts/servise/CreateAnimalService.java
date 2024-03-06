0ba51f8ced1327e78eee219b255802836bb4b963
SashaShreider <sasha20032205@mail.ru>
2024-03-06 15:02:53 +0300

    Минорные изменения

--- create-animals-starter/src/main/java/ru/mts/servise/CreateAnimalService.java
+++ create-animals-starter/src/main/java/ru/mts/servise/CreateAnimalService.java
@@ -1,10 +1,11 @@
 package ru.mts.servise;
 
+import org.springframework.stereotype.Component;
 import ru.mts.entity.Animal;
 
 import java.util.List;
 
-
+@Component
 public interface CreateAnimalService {
     /**
      * Создание 10 случайных животных
